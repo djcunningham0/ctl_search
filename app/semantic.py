12c2ab23b04a821bc893d20d794669415732cf85
djcunningham0 <djcunningham0@gmail.com>
2025-10-12 14:20:48 -0500

    add models; use encode_query and encode_document

--- app/semantic.py
+++ app/semantic.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import polars as pl
 import streamlit as st
 from sentence_transformers import SentenceTransformer
@@ -8,6 +10,7 @@ from app.sql import execute_query
 
 DEFAULT_EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 
+# https://sbert.net/docs/sentence_transformer/pretrained_models.html#semantic-search-models
 SUGGESTED_MODELS = [
     "all-MiniLM-L6-v2",
     "multi-qa-mpnet-base-cos-v1",
@@ -15,6 +18,16 @@ SUGGESTED_MODELS = [
     "all-mpnet-base-v2",  # surprisingly seems worse than all-MiniLM-L6-v2
     "multi-qa-distilbert-cos-v1",
     "multi-qa-MiniLM-L6-cos-v1",
+
+    # MSMARCO models are designed to work better for asymmetric search, where queries are shorter than documents
+    # https://sbert.net/docs/pretrained-models/msmarco-v5.html
+    "msmarco-MiniLM-L6-cos-v5",  # cosine
+    "msmarco-MiniLM-L12-cos-v5",  # cosine
+    "msmarco-distilbert-cos-v5",  # cosine
+    "msmarco-distilbert-base-tas-b",  # dot
+    "msmarco-distilbert-dot-v5",  # dot
+    "msmarco-bert-base-dot-v5",  # dot
+
     # I considered the ones below this line but they performed worse than the others
     # "all-distilroberta-v1",
     # "all-MiniLM-L12-v2",
@@ -33,9 +46,9 @@ class SemanticSearch:
         return _embed_corpus(self.embedder, self.corpus)
 
     def encode_query(self, query: str) -> Tensor:
-        return self.embedder.encode(query, convert_to_tensor=True)
+        return self.embedder.encode_query(query, convert_to_tensor=True)  # type: ignore
 
-    def search(self, query: str, n: int = None) -> pl.DataFrame:
+    def search(self, query: str, n: Optional[int] = None) -> pl.DataFrame:
         query_embedding = self.encode_query(query)
         cos_scores: Tensor = self.embedder.similarity(query_embedding, self.embedded_corpus)[0]
         n = n or len(self.corpus)
@@ -84,4 +97,4 @@ def load_sentence_transformer_model(model_str: str) -> SentenceTransformer:
 
 @st.cache_data(hash_funcs={SentenceTransformer: lambda x: x.model_card_data.base_model})
 def _embed_corpus(embedder: SentenceTransformer, corpus: list[str]) -> Tensor:
-    return embedder.encode(corpus, convert_to_tensor=True)
+    return embedder.encode_document(corpus, convert_to_tensor=True)  # type: ignore
